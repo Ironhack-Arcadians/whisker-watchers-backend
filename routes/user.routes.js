acb9cb8597120a3dc630a36648e80ad68c8dce97
Vicente Duch <vhduch@gmail.com>
2024-11-20 11:13:45 +0100

    add user route

---
+++ routes/user.routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../middleware/jwt.middleware");
+
+
+// GET /user/profile//  user by Id
+
+router.get("/profile", User.isAuthenticated, (req, res, next) => {
+    const userId = req.payload._id;
+    
+    User.findById(userId)
+    .then(user => {
+        if (!user) {
+            return res.status(404).json({message: "User not found"})
+        }
+        const { email, name, role, profile_pic, location, bio } = user;
+        res.status(200).json({user: {email, name, role, profile_pic, location, bio }});
+    })
+    .catch(err => res.status(500).json({ message: "Error retrieving user profile", error: err}));
+    
+});
+
+module.exports = router;
