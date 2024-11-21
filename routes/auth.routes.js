2116b5410ecdc9ffd4cb573c54b13fedfd4d0cdb
Vicente Duch <vhduch@gmail.com>
2024-11-21 15:24:10 +0100

    fix field functionality for signup form

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,7 +18,7 @@ const saltRounds = 10;
 
 // POST /auth/signup  - Creates a new user in the database
 router.post("/signup", (req, res, next) => {
-  const { email, password, name, role } = req.body;
+  const { email, name, role, password, phone, profile_pic, location, bio} = req.body;
 
   // Check if email or password or name are provided as empty strings
   if (email === "" || password === "" || name === "" || role === "") {
@@ -58,15 +58,15 @@ router.post("/signup", (req, res, next) => {
 
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then`
-      return User.create({ email, password: hashedPassword, name, role });
+      return User.create({ email, password: hashedPassword, name, role, profile_pic, bio, phone, location });
     })
     .then((createdUser) => {
       // Deconstruct the newly created user object to omit the password
       // We should never expose passwords publicly
-      const { email, name, role,  _id } = createdUser;
+      const { email, name, role,  _id, bio, location, phone, profile_pic } = createdUser;
 
       // Create a new object that doesn't expose the password
-      const user = { email, name, role,  _id };
+      const user = { email, name, role,  _id, bio, location, phone, profile_pic };
 
       // Send a json response containing the user object
       res.status(201).json({ user: user });
